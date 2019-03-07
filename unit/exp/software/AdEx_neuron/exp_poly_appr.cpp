f038828cf134d2d56dfcc25ed4cfd2124c0f2d48
wudidaizi <wudidaizi@wudidaizideMacBook-Pro.local>
2019-03-07 15:01:41 -0600

    approximate exp_poly_appr 1

--- unit/exp/software/AdEx_neuron/exp_poly_appr.cpp
+++ unit/exp/software/AdEx_neuron/exp_poly_appr.cpp
@@ -10,12 +10,68 @@ void EXP_POLY_APPR::Init(vector<double> param1, vector<double> param2, uint64_t 
     input = 0;
     output = 0;
     term_cnt = coeff.size();
-
+    mae = 0;
+    mse = 0;
+    mre = 0;
+    wce = 0;
+    wcre = 0;
     poly_appr.Init(coeff, order, fracWidth, fixed_sim);
 
     // exp(1) is around 2.7, requiring 2 bits integer, so have format of {2-bit.(fracWidth-2)-bit}
     truncation.Init(fracWidth-2);
     exp1 = truncation.Out(exp(1));
+
+    totalCnt = 1UL<<fracWidth;
+    // printf("%d\n", totalCnt);
+    N = 64;
+
+    weight.resize(totalCnt);
+    for (int i = 0; i < totalCnt; ++i)
+    {
+        weight[i] = 0;
+    }
+    
+    // // Gaussian distribution
+    // double number;
+    // default_random_engine generator;
+    // normal_distribution<double> distribution(0.25,0.1);
+    // for (int i = 0; i < (totalCnt)*N; ++i)
+    // {
+    //     number = distribution(generator);
+    //     if ((number>=0.0) && (number<1.0))
+    //     {
+    //         ++weight[int(number*totalCnt)];
+    //     }
+    // }
+
+    // uniform distribution
+    for (int i = 0; i < (totalCnt); ++i)
+    {
+        weight[i] = N;
+    }
+
+    // step distribution
+    // for (int i = 0; i < (totalCnt); ++i)
+    // {
+    //     if (i < totalCnt/2)
+    //     {
+    //         weight[i] = 0;
+    //     }
+    //     else
+    //     {
+    //         weight[i] = N*2;
+    //     }
+    // }
+
+    // for (int i = 0; i < (totalCnt); ++i)
+    // {
+    //     printf("%d\n", weight[i]);
+    //     for (int j = 0; j < weight[i]; ++j)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
 
 }
 
@@ -94,4 +150,87 @@ double EXP_POLY_APPR::Out(double param1)
     }
     // output at this point has format {2.fracWidth-2}
     return output;
+}
+
+void EXP_POLY_APPR::Eval()
+{
+    mae = 0;
+    mse = 0;
+    mre = 0;
+    wce = 0;
+    wcre = 0;
+    double appr;
+    double orig;
+    double in;
+    double absError;
+
+    for (int i = 0; i < (totalCnt); ++i)
+    {
+        in = (double)(i/pow(2,fracWidth));
+        orig = exp(in);
+        // appr at this point has format {2.fracWidth}
+        if (in < threshold)
+        {
+            // printf("less\n");
+            appr = poly_appr.Out(in);
+        }
+        else
+        {
+            // printf("more\n");
+            // printf("%lf\n", poly_appr.Out(in-1));
+            appr = poly_appr.Out(in-1);
+        }
+
+        if (in < threshold)
+        {
+            // {2,fracWidth-2} appr multiply by e^0 with format {2,fracWidth-2}
+            appr = truncation.Out(appr);
+        }
+        else
+        {
+            // {2,fracWidth-2} appr multiply by e^1 with format {2,fracWidth-2}
+            appr = mul_fixed.Out(truncation.Out(appr), exp1, fracWidth-2);
+        }
+
+        absError = abs(appr - orig);
+        mae += absError * weight[i];
+        mse += pow(absError,2.0) * weight[i];
+        mre += absError/orig * weight[i];
+        if (wce < absError)
+        {
+            wce = absError;
+        }
+        if (wcre < absError/orig)
+        {
+            wcre = absError/orig;
+        }
+    }
+    mae /= (pow(2,fracWidth)*N);
+    mse /= (pow(2,fracWidth)*N);
+    mre /= (pow(2,fracWidth)*N);
+}
+
+double EXP_POLY_APPR::MAE()
+{
+    return mae;
+}
+
+double EXP_POLY_APPR::MSE()
+{
+    return mse;
+}
+
+double EXP_POLY_APPR::MRE()
+{
+    return mre;
+}
+
+double EXP_POLY_APPR::WCE()
+{
+    return wce;
+}
+
+double EXP_POLY_APPR::WCRE()
+{
+    return wcre;
 }
